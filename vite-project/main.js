d790caae068bb630966e3c96d6e55b3737598ba1
noaha90 <noahabb@outlook.com>
2024-01-09 12:41:18 -0500

    ea
    spot

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,22 +1,33 @@
 import './style.css'
 
-async function APICall(){
+async function mainCall(){
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`)
   const data = await response.json(); 
-  console.log(data)
   data.categories.forEach(catergory => {
-    console.log(catergory.strCategory)
-    console.log(document.getElementById("categories"))
     document.getElementById("categories").insertAdjacentHTML("afterbegin",`
-    <input type="checkbox"  name="${catergory.strCategory}" value="="${catergory.strCategory}">
+    <input type="checkbox" class="box" value="${catergory.strCategory}">
     <label for="${catergory.strCategory}">${catergory.strCategory}</label><br>`
     )});
+    document.getElementById("call").addEventListener("click", (event) => {
+      event.preventDefault()
+      document.getElementById("gal").innerHTML = ""
+      document.querySelectorAll(".box").forEach(box => {
+        if(box.checked){
+           APICall(box.value)  
+        }
+        })
+        console.log(document.getElementsByTagName('img'))
+        document.getElementsByTagName('img').forEach(img => {console.log(img)})
+    }); 
+  }
+
+
+async function APICall(input){
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${input}`)
+  const data = await response.json(); 
+  console.log(data)
+  document.getElementById("gal").insertAdjacentHTML("beforeend",`<h2>${input}</h2>`)
+  data.meals.forEach(dish => {document.getElementById("gal").insertAdjacentHTML("beforeend",`<p>${dish.strMeal}</p><img class="dish" src="${dish.strMealThumb}">`)})
 }
 
-APICall()
-
-document.getElementById("call").addEventListener("click", (event) => {
-  event.preventDefault()
-  console.log("ea")
-});
-
+mainCall()
